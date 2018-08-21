16ce0555e950ec913528a2c4ff1c123543d03f8f
TheRedstoneDaim <joeljacob500@gmail.com>
2018-08-21 17:14:25 -0400

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-
-from Reverser import Reverser
-
-wordToBeReversed = input("Enter a sentence which you want to reverse: ")
-reverser = Reverser(wordToBeReversed)
-print("The reversed sentence is:", reverser.reverse())
